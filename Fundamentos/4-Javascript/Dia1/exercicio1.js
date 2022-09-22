b8d7e25205b6b9633aa2b75870e5a672d90da983
Pedreek <pedreekdev@gmail.com>
2022-09-22 15:09:46 -0300

    exercicios js

---
+++ Fundamentos/4-Javascript/Dia1/exercicio1.js
@@ -0,0 +1,11 @@
+const myName = 'Pedro';
+const birthCity = 'Montes Claros';
+
+let birthYear = 1992;
+
+birthYear = 1993;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
---
+++ Fundamentos/4-Javascript/Dia1/exercicio3.js
@@ -0,0 +1,21 @@
+
+// const grade = 80;
+// if (grade >= 80) {
+//   console.log("Parabéns, você foi aprovada(o)!");
+// } else if (grade < 80 && grade >= 60) {
+//   console.log("Você está na nossa lista de espera");
+// } else if (grade < 60) {
+//   console.log("Você foi reprovada(o)");
+// }
+
+const nota = 67;
+
+if (nota >= 80) {
+    console.log('Aprovado!');
+} else if (nota < 80 && nota >= 60) {
+    console.log('Lista de espera!');
+} else if (nota < 60) {
+    console.log('reprovado!');
+}
+
+
