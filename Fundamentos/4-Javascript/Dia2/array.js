b1e37cf8d64b082daf61ad7f40a0d94b1a778f4f
Pedreek <pedreekdev@gmail.com>
2022-09-23 03:08:53 -0300

    exercio dia 2

---
+++ Fundamentos/4-Javascript/Dia2/array.js
@@ -0,0 +1,14 @@
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+for (let index = 0; index < groceryList.lenght; index += 1);
+    console.log(groceryList.sort());
+
+/* =======================================================================*/
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+for (let nomes of names);
+nomes = +1;
+
+console.log(nomes);
