0ff5cfdd39655af0689c0afe34a4d32f1affe7eb
Pedreek <pedreekdev@gmail.com>
2022-09-27 02:44:00 -0300

    esqueci de dar commit nesse

--- Fundamentos/4-Javascript/Dia2/array.js
+++ Fundamentos/4-Javascript/Dia2/array.js
@@ -4,11 +4,23 @@ let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
 for (let index = 0; index < groceryList.lenght; index += 1);
     console.log(groceryList.sort());
 
-/* =======================================================================*/
+/*=======================================================================*/
 
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
 
-for (let nomes of names);
-nomes = +1;
+console.log(menuServices);
 
-console.log(nomes);
+/*=======================================================================*/
+
+let meni = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = meni.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+/*=======================================================================*/
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+    menu2.push('Contato');
+
+console.log(menu2);
