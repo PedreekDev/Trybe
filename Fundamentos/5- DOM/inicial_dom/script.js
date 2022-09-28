ac89e6f93e3da3f7a946971eb8ed32573b06e7a2
Pedreek <pedreekdev@gmail.com>
2022-09-28 20:58:38 -0300

    novas pastas DOM

---
+++ Fundamentos/5- DOM/inicial_dom/script.js
@@ -0,0 +1,16 @@
+/* ==================== CoresBG ==================== */
+
+document.getElementById('container').style.background = '#E5E4E2';
+document.getElementById('header-container').style.background = '#54c96d';
+
+document.getElementsByTagName('div')[0].style.background = 'pink';
+document.getElementsByTagName('div')[1].style.background = 'pink';
+document.getElementsByTagName('div')[2].style.background = 'rgb(218, 219, 113)'; 
+document.getElementsByTagName('div')[3].style.background = 'rgb(218, 219, 113)' ;
+
+document.getElementsByTagName('h3')[0].style.background = 'purple';
+document.getElementsByTagName('h3')[1].style.background = 'purple';
+document.getElementsByTagName('h3')[2].style.background = 'black';
+document.getElementsByTagName('h3')[3].style.background = 'black';
+
+document.querySelector('footer').style.background = 'rgb(35, 77, 61)';
