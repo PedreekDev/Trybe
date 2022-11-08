efb11f689bab5d6ce62ae7d701f8120ec57692a7
Pedreek <pedreekdev@gmail.com>
2022-11-08 18:48:34 -0300

    deus queira que deu

--- Front-End/Intro FrontEnd/Dia1/src/main.js
+++ Front-End/Intro FrontEnd/Dia1/src/main.js
@@ -1,9 +1,19 @@
 import { nanoid } from 'nanoid';
 import './style.css';
+import copy from 'clipboard-copy';
 
 const getButton = document.querySelector('button');
 const display = document.querySelector('h2');
 
 getButton.addEventListener('click', () => {
     display.innerHTML = nanoid();
+});
+
+display.addEventListener('click', async (event) => {
+    await copy(event.target.innerHTML);
+    alert('Senha copiada!')
 })
+
+
+
+
