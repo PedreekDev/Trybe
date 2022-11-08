82048b8a8d3d8057257020b6f034de5244c9220d
Pedreek <pedreekdev@gmail.com>
2022-11-08 18:08:18 -0300

    projeto feito senhas

--- Front-End/Intro FrontEnd/Dia1/src/main.js
+++ Front-End/Intro FrontEnd/Dia1/src/main.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import { nanoid } from 'nanoid';
+import './style.css';
+
+const getButton = document.querySelector('button');
+const display = document.querySelector('h2');
+
+getButton.addEventListener('click', () => {
+    display.innerHTML = nanoid();
+})
